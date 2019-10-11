ab5cdab6127898e1b9a58f52d20ac86cc78d1de4
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-10-11 16:35:07 +0100

    Update cached bank holidays list

--- govuk_bank_holidays/__init__.py
+++ govuk_bank_holidays/__init__.py
@@ -1,3 +1,3 @@
-VERSION = (0, 4)
+VERSION = (0, 5)
 __version__ = '.'.join(map(str, VERSION))
-__author__ = 'Ministry of Justice Digital Services'
+__author__ = 'Ministry of Justice Digital & Technology'
