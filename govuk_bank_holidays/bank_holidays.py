571b29e59ae11e73315827d394420cbddf08e737
Jerome Riga <jriga@zemis.co.uk>
2021-08-17 13:07:32 +0100

    Refactoring lib
    * Use `operator.itemgetter`
    
    * Use generators to get holidays and work_days

--- govuk_bank_holidays/bank_holidays.py
+++ govuk_bank_holidays/bank_holidays.py
@@ -3,11 +3,12 @@ import functools
 import gettext
 import json
 import logging
+import operator
 import os
 
 import requests
 
-__all__ = ('BankHolidays',)
+__all__ = ('BankHolidays', )
 logger = logging.getLogger(__name__)
 
 
@@ -26,9 +27,14 @@ class BankHolidays:
     SCOTLAND = 'scotland'
     NORTHERN_IRELAND = 'northern-ireland'
 
+    # direction contants
+    FUTURE = 'future'
+    PAST = 'past'
+
     @classmethod
     def load_backup_data(cls):
-        with open(os.path.join(os.path.dirname(__file__), 'bank-holidays.json')) as f:
+        with open(os.path.join(os.path.dirname(__file__),
+                               'bank-holidays.json')) as f:
             return json.load(f)
 
     def __init__(self, locale=None, weekend=(5, 6), use_cached_holidays=False):
@@ -43,15 +49,20 @@ class BankHolidays:
             data = self.load_backup_data()
         else:
             try:
-                logger.debug('Downloading bank holidays from %s' % self.source_url)
+                logger.debug('Downloading bank holidays from %s' %
+                             self.source_url)
                 data = requests.get(self.source_url).json()
             except (requests.RequestException, ValueError):
                 logger.warning('Using backup bank holiday data')
                 data = self.load_backup_data()
 
         if locale:
-            trans = gettext.translation('messages', fallback=True, languages=[locale],
-                                        localedir=os.path.join(os.path.dirname(__file__), 'locale'))
+            trans = gettext.translation('messages',
+                                        fallback=True,
+                                        languages=[locale],
+                                        localedir=os.path.join(
+                                            os.path.dirname(__file__),
+                                            'locale'))
         else:
             trans = gettext.NullTranslations()
         trans = trans.ugettext if hasattr(trans, 'ugettext') else trans.gettext
@@ -64,18 +75,26 @@ class BankHolidays:
         def map_holiday(holiday):
             try:
                 return {
-                    'title': _(holiday['title']),
-                    'date': datetime.datetime.strptime(holiday['date'], '%Y-%m-%d').date(),
-                    'notes': _(holiday.get('notes', '')),
-                    'bunting': bool(holiday.get('bunting')),
+                    'title':
+                    _(holiday['title']),
+                    'date':
+                    datetime.datetime.strptime(holiday['date'],
+                                               '%Y-%m-%d').date(),
+                    'notes':
+                    _(holiday.get('notes', '')),
+                    'bunting':
+                    bool(holiday.get('bunting')),
                 }
             except (KeyError, ValueError):
                 logger.warning('Holiday could not be parsed')
                 logger.debug(holiday, exc_info=True)
 
         self.data = {
-            division: sorted(filter(None, map(map_holiday, item.get('events', []))),
-                             key=lambda e: e['date'])
+            division: sorted([
+                holiday for holiday in map(map_holiday, item.get('events', []))
+                if holiday
+            ],
+                             key=operator.itemgetter('date'))
             for division, item in data.items()
         }
 
@@ -84,31 +103,95 @@ class BankHolidays:
         Iterates over the current year's holidays that are common to *all* divisions
         :return: list of dicts with titles, dates, etc
         """
-        return iter(self.get_holidays(year=datetime.date.today().year))
+        start = datetime.date(datetime.date.today().year, 1, 1)
+        end = datetime.date(datetime.date.today().year, 12, 31)
+
+        return self.holidays(start=start, end=end)
+
+    def _date_or_today(self, date=None):
+        """
+        Returns the current date when date is omitted
+        """
+        return date or datetime.date.today()
+
+    def _get_common_holidays(self):
+        """
+        Returns an iterable of the common holidays
+        """
+        dates_in_common = functools.reduce(
+            set.intersection,
+            (set(map(operator.itemgetter('date'), division_holidays))
+             for _, division_holidays in self.data.items()))
+
+        holidays = filter(lambda holiday: holiday['date'] in dates_in_common,
+                          self.data[self.ENGLAND_AND_WALES])
+
+        return list(holidays)
+
+    def holidays(self,
+                 division=None,
+                 start=datetime.date.today(),
+                 end=None,
+                 direction=FUTURE):
+        """
+        Returns a generator for holidays
+        by division
+        from a specific date
+        direction future or past
+        """
+        dataset = self.data.get(
+            division) if division else self._get_common_holidays()
+
+        def valid_future_dates(e):
+            return e['date'] >= start if end is None else e[
+                'date'] >= start and e['date'] <= end
+
+        def valid_past_dates(e):
+            return e['date'] <= start if end is None else e[
+                'date'] <= start and e['date'] >= end
+
+        source = filter(valid_future_dates,
+                        dataset) if direction == self.FUTURE else filter(
+                            valid_past_dates, reversed(dataset))
+
+        for day in source:
+            yield day
+
+    def work_days(self,
+                  division=None,
+                  start=datetime.date.today(),
+                  end=None,
+                  direction=FUTURE):
+        """
+        Returns a generator for working days
+        """
+        _holidays = map(
+            operator.itemgetter('date'),
+            self.holidays(division=division,
+                          direction=direction,
+                          start=start,
+                          end=end))
+        day = 0
+
+        while True:
+            day += 1
+            delta = datetime.timedelta(days=day)
+            current = start + delta if direction == self.FUTURE else start - delta
+
+            if current.weekday(
+            ) not in self.weekend and current not in _holidays:
+                yield current
 
     def get_holidays(self, division=None, year=None):
         """
-        Gets a list of all known bank holidays, optionally filtered by division and/or year
-        NB: If no division is specified, only holidays common to *all* divisions are returned.
-        :param division: see division constants; defaults to common holidays
-        :param year: defaults to all available years
-        :return: list of dicts with titles, dates, etc
+        Returns an iterable of holidays
+        current year when year is omitted
         """
-        if division:
-            holidays = self.data[division]
-        else:
-            holidays = self.data[self.ENGLAND_AND_WALES]
-            dates_in_common = functools.reduce(
-                set.intersection,
-                (
-                    set(map(lambda holiday: holiday['date'], division_holidays))
-                    for division, division_holidays in self.data.items()
-                )
-            )
-            holidays = filter(lambda holiday: holiday['date'] in dates_in_common, holidays)
-        if year:
-            holidays = filter(lambda holiday: holiday['date'].year == year, holidays)
-        return list(holidays)
+        current_year = year if year is not None else datetime.date.today().year
+        start = datetime.date(current_year, 1, 1)
+        end = datetime.date(current_year, 12, 31)
+
+        return list(self.holidays(division=division, start=start, end=end))
 
     def is_holiday(self, date, division=None):
         """
@@ -118,7 +201,8 @@ class BankHolidays:
         :param division: see division constants; defaults to common holidays
         :return: bool
         """
-        return date in (holiday['date'] for holiday in self.get_holidays(division=division))
+        return date in map(operator.itemgetter('date'),
+                           self.holidays(division=division, start=date))
 
     def is_work_day(self, date, division=None):
         """
@@ -128,9 +212,8 @@ class BankHolidays:
         :param division: see division constants; defaults to common holidays
         :return: bool
         """
-        return date.weekday() not in self.weekend and date not in (
-            holiday['date'] for holiday in self.get_holidays(division=division)
-        )
+        return date.weekday() not in self.weekend and not self.is_holiday(
+            date, division=division)
 
     def get_next_holiday(self, division=None, date=None):
         """
@@ -140,10 +223,9 @@ class BankHolidays:
         :param date: search starting from this date; defaults to today
         :return: dict or None
         """
-        date = date or datetime.date.today()
-        for holiday in self.get_holidays(division=division):
-            if holiday['date'] > date:
-                return holiday
+        return next(
+            self.holidays(division=division,
+                          start=self._date_or_today(date=date)))
 
     def get_prev_holiday(self, division=None, date=None):
         """
@@ -153,10 +235,10 @@ class BankHolidays:
         :param date: search starting from this date; defaults to today
         :return: dict or None
         """
-        date = date or datetime.date.today()
-        for holiday in reversed(self.get_holidays(division=division)):
-            if holiday['date'] < date:
-                return holiday
+        return next(
+            self.holidays(division=division,
+                          start=self._date_or_today(date=date),
+                          direction=self.PAST))
 
     def get_next_work_day(self, division=None, date=None):
         """
@@ -166,13 +248,10 @@ class BankHolidays:
         :param date: search starting from this date; defaults to today
         :return: datetime.date; NB: get_next_holiday returns a dict
         """
-        date = date or datetime.date.today()
-        one_day = datetime.timedelta(days=1)
-        holidays = set(holiday['date'] for holiday in self.get_holidays(division=division))
-        while True:
-            date += one_day
-            if date.weekday() not in self.weekend and date not in holidays:
-                return date
+        return next(
+            self.work_days(division=division,
+                           start=self._date_or_today(date=date),
+                           direction=self.FUTURE))
 
     def get_prev_work_day(self, division=None, date=None):
         """
@@ -182,10 +261,7 @@ class BankHolidays:
         :param date: search starting from this date; defaults to today
         :return: datetime.date; NB: get_next_holiday returns a dict
         """
-        date = date or datetime.date.today()
-        one_day = datetime.timedelta(days=1)
-        holidays = set(holiday['date'] for holiday in self.get_holidays(division=division))
-        while True:
-            date -= one_day
-            if date.weekday() not in self.weekend and date not in holidays:
-                return date
+        return next(
+            self.work_days(division=division,
+                           start=self._date_or_today(date=date),
+                           direction=self.PAST))
--- tests/test_bank_holidays.py
+++ tests/test_bank_holidays.py
@@ -18,55 +18,71 @@ class BankHolidayTestCase(unittest.TestCase):
     @classmethod
     def get_bank_holidays_using_local_data(cls, **kwargs):
         with responses.RequestsMock() as rsps:
-            rsps.add(rsps.GET, BankHolidays.source_url, json=BankHolidays.load_backup_data())
+            rsps.add(rsps.GET,
+                     BankHolidays.source_url,
+                     json=BankHolidays.load_backup_data())
             return BankHolidays(**kwargs)
 
     def assertExpectedFormat(self, holidays):  # noqa: N802
         last_holiday = None
         expected_keys = ['bunting', 'date', 'notes', 'title']
         for holiday in holidays:
-            self.assertListEqual(sorted(holiday.keys()), expected_keys,
+            self.assertListEqual(sorted(holiday.keys()),
+                                 expected_keys,
                                  msg='Unexpected or missing dictionary keys')
             if last_holiday:
-                self.assertGreater(holiday['date'], last_holiday['date'],
+                self.assertGreater(holiday['date'],
+                                   last_holiday['date'],
                                    msg='Holidays are not correctly sorted')
             last_holiday = holiday
 
     def test_holidays(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
-        holidays = bank_holidays.get_holidays()
+        holidays = list(
+            bank_holidays.holidays(start=datetime.date(2012, 1, 1)))
         self.assertEqual(len(holidays), BACKUP_DATA_COMMON_HOLIDAY_COUNT)
         self.assertExpectedFormat(holidays)
 
     def test_holidays_for_division(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
-        holidays = bank_holidays.get_holidays(division=BankHolidays.ENGLAND_AND_WALES)
-        self.assertEqual(len(holidays), BACKUP_DATA_ENGLAND_AND_WALES_HOLIDAY_COUNT)
+        holidays = list(
+            bank_holidays.holidays(start=datetime.date(2012, 1, 1),
+                                   division=BankHolidays.ENGLAND_AND_WALES))
+        self.assertEqual(len(holidays),
+                         BACKUP_DATA_ENGLAND_AND_WALES_HOLIDAY_COUNT)
         self.assertExpectedFormat(holidays)
-        holidays = bank_holidays.get_holidays(division=BankHolidays.SCOTLAND)
+        holidays = list(
+            bank_holidays.holidays(start=datetime.date(2012, 1, 1),
+                                   division=BankHolidays.SCOTLAND))
         self.assertEqual(len(holidays), BACKUP_DATA_SCOTLAND_HOLIDAY_COUNT)
         self.assertExpectedFormat(holidays)
-        self.assertIn('St Andrew’s Day', map(lambda holiday: holiday['title'], holidays))
+        self.assertIn('St Andrew’s Day',
+                      map(lambda holiday: holiday['title'], holidays))
 
     def test_holidays_for_year(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         holidays = bank_holidays.get_holidays(year=2017)
         self.assertEqual(len(holidays), 6)
         self.assertExpectedFormat(holidays)
-        self.assertTrue(all(holiday['date'].year == 2017 for holiday in holidays))
+        self.assertTrue(
+            all(holiday['date'].year == 2017 for holiday in holidays))
 
     def test_holidays_for_division_and_year(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
-        holidays = bank_holidays.get_holidays(division=BankHolidays.NORTHERN_IRELAND, year=2016)
+        holidays = bank_holidays.get_holidays(
+            division=BankHolidays.NORTHERN_IRELAND, year=2016)
         self.assertEqual(len(holidays), 10)
         self.assertExpectedFormat(holidays)
-        self.assertIn('Battle of the Boyne (Orangemen’s Day)', map(lambda holiday: holiday['title'], holidays))
-        self.assertTrue(all(holiday['date'].year == 2016 for holiday in holidays))
+        self.assertIn('Battle of the Boyne (Orangemen’s Day)',
+                      map(lambda holiday: holiday['title'], holidays))
+        self.assertTrue(
+            all(holiday['date'].year == 2016 for holiday in holidays))
 
     def test_holiday_iterator(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         holidays_1 = [holiday for holiday in bank_holidays]
-        holidays_2 = bank_holidays.get_holidays(year=datetime.date.today().year)
+        holidays_2 = bank_holidays.get_holidays(
+            year=datetime.date.today().year)
         self.assertListEqual(holidays_1, holidays_2)
 
     def test_next_bank_holiday(self):
@@ -74,90 +90,88 @@ class BankHolidayTestCase(unittest.TestCase):
         second_january = datetime.date(2016, 1, 2)
         self.assertEqual(
             bank_holidays.get_next_holiday(date=second_january)['date'],
-            datetime.date(2016, 3, 25)
-        )
+            datetime.date(2016, 3, 25))
         self.assertEqual(
-            bank_holidays.get_next_holiday(division=BankHolidays.SCOTLAND, date=second_january)['date'],
-            datetime.date(2016, 1, 4)
-        )
+            bank_holidays.get_next_holiday(division=BankHolidays.SCOTLAND,
+                                           date=second_january)['date'],
+            datetime.date(2016, 1, 4))
 
     def test_prev_bank_holiday(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         fifth_january = datetime.date(2016, 1, 5)
         self.assertEqual(
             bank_holidays.get_prev_holiday(date=fifth_january)['date'],
-            datetime.date(2016, 1, 1)
-        )
+            datetime.date(2016, 1, 1))
         self.assertEqual(
-            bank_holidays.get_prev_holiday(division=BankHolidays.SCOTLAND, date=fifth_january)['date'],
-            datetime.date(2016, 1, 4)
-        )
+            bank_holidays.get_prev_holiday(division=BankHolidays.SCOTLAND,
+                                           date=fifth_january)['date'],
+            datetime.date(2016, 1, 4))
 
     def test_is_holiday_check(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         self.assertTrue(bank_holidays.is_holiday(datetime.date(2012, 1, 2)))
         self.assertFalse(bank_holidays.is_holiday(datetime.date(2016, 1, 4)))
-        self.assertTrue(bank_holidays.is_holiday(datetime.date(2016, 1, 4), division=BankHolidays.SCOTLAND))
+        self.assertTrue(
+            bank_holidays.is_holiday(datetime.date(2016, 1, 4),
+                                     division=BankHolidays.SCOTLAND))
 
     def test_next_work_day(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         self.assertEqual(
             bank_holidays.get_next_work_day(date=datetime.date(2017, 12, 19)),
-            datetime.date(2017, 12, 20)
-        )
+            datetime.date(2017, 12, 20))
         self.assertEqual(
             bank_holidays.get_next_work_day(date=datetime.date(2017, 12, 22)),
-            datetime.date(2017, 12, 27)
-        )
+            datetime.date(2017, 12, 27))
         self.assertEqual(
             bank_holidays.get_next_work_day(date=datetime.date(2017, 12, 30)),
-            datetime.date(2018, 1, 2)
-        )
+            datetime.date(2018, 1, 2))
         self.assertEqual(
-            bank_holidays.get_next_work_day(division=BankHolidays.SCOTLAND, date=datetime.date(2017, 12, 30)),
-            datetime.date(2018, 1, 3)
-        )
+            bank_holidays.get_next_work_day(division=BankHolidays.SCOTLAND,
+                                            date=datetime.date(2017, 12, 30)),
+            datetime.date(2018, 1, 3))
 
     def test_prev_work_day(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         self.assertEqual(
             bank_holidays.get_prev_work_day(date=datetime.date(2017, 12, 19)),
-            datetime.date(2017, 12, 18)
-        )
+            datetime.date(2017, 12, 18))
         self.assertEqual(
             bank_holidays.get_prev_work_day(date=datetime.date(2017, 12, 18)),
-            datetime.date(2017, 12, 15)
-        )
+            datetime.date(2017, 12, 15))
         self.assertEqual(
             bank_holidays.get_prev_work_day(date=datetime.date(2017, 12, 27)),
-            datetime.date(2017, 12, 22)
-        )
+            datetime.date(2017, 12, 22))
         self.assertEqual(
             bank_holidays.get_prev_work_day(date=datetime.date(2018, 1, 3)),
-            datetime.date(2018, 1, 2)
-        )
+            datetime.date(2018, 1, 2))
         self.assertEqual(
-            bank_holidays.get_prev_work_day(division=BankHolidays.SCOTLAND, date=datetime.date(2018, 1, 3)),
-            datetime.date(2017, 12, 29)
-        )
+            bank_holidays.get_prev_work_day(division=BankHolidays.SCOTLAND,
+                                            date=datetime.date(2018, 1, 3)),
+            datetime.date(2017, 12, 29))
 
     def test_is_work_day(self):
         bank_holidays = self.get_bank_holidays_using_local_data()
         self.assertTrue(bank_holidays.is_work_day(datetime.date(2017, 12, 19)))
         self.assertFalse(bank_holidays.is_work_day(datetime.date(2018, 1, 1)))
         self.assertTrue(bank_holidays.is_work_day(datetime.date(2018, 1, 2)))
-        self.assertFalse(bank_holidays.is_work_day(datetime.date(2018, 1, 2), division=BankHolidays.SCOTLAND))
+        self.assertFalse(
+            bank_holidays.is_work_day(datetime.date(2018, 1, 2),
+                                      division=BankHolidays.SCOTLAND))
 
     def test_configuring_weekends(self):
-        bank_holidays = self.get_bank_holidays_using_local_data(weekend={1, 5, 6})
-        self.assertFalse(bank_holidays.is_work_day(datetime.date(2017, 12, 19)))
+        bank_holidays = self.get_bank_holidays_using_local_data(
+            weekend={1, 5, 6})
+        self.assertFalse(bank_holidays.is_work_day(datetime.date(2017, 12,
+                                                                 19)))
 
     @mock.patch('govuk_bank_holidays.bank_holidays.logger')
     def test_holidays_use_backup_data(self, mock_logger):
         with responses.RequestsMock() as rsps:
             rsps.add(rsps.GET, BankHolidays.source_url, status=404)
             bank_holidays = BankHolidays()
-        mock_logger.warning.assert_called_once_with('Using backup bank holiday data')
+        mock_logger.warning.assert_called_once_with(
+            'Using backup bank holiday data')
         self.assertTrue(bank_holidays.get_holidays())
 
     @mock.patch('govuk_bank_holidays.bank_holidays.logger')
@@ -169,7 +183,8 @@ class BankHolidayTestCase(unittest.TestCase):
 
     def test_localisation(self):
         bank_holidays = self.get_bank_holidays_using_local_data(locale='cy')
-        holidays = bank_holidays.get_holidays(division=BankHolidays.ENGLAND_AND_WALES)
+        holidays = bank_holidays.get_holidays(
+            division=BankHolidays.ENGLAND_AND_WALES)
         holiday_names = set(holiday['title'] for holiday in holidays)
         self.assertIn('Dydd Nadolig', holiday_names)
         self.assertNotIn('Christmas Day', holiday_names)
