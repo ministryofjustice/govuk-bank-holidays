5ad9b7b086865aa779e9a087555f841df80a858d
Igor <igor.ushkarev1@justice.gov.uk>
2025-06-05 14:43:57 +0100

    Remove (official) support and testing on python < 3.9 (though it should continue working with a warning message) and add testing for 3.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import warnings
 
 from setuptools import setup
 
-if sys.version_info[0:2] < (3, 7):
-    warnings.warn('This package is only tested on Python version 3.7+', stacklevel=1)
+if sys.version_info[0:2] < (3, 9):
+    warnings.warn('This package is only tested on Python version 3.9+', stacklevel=1)
 
 setup()
